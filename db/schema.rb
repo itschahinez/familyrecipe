304d761304d3aae9a075a678c5654a94e22933fa
Chahinez Haddad <mackychahinez@gmail.com>
2022-08-31 12:00:42 +0200

    fixed preptime in create new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_095231) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_090450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,9 +78,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_095231) do
     t.text "description"
     t.integer "category"
     t.bigint "creator_id"
-    t.integer "prep_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "preptime_hour"
+    t.integer "preptime_mn"
+    t.integer "prep_time"
     t.index ["creator_id"], name: "index_recipes_on_creator_id"
   end
 
