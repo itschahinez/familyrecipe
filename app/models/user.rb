76f8b89240112077c59eb0375c5cdd7cd4060c24
Olivia Bonnal <oliviabonnal@gmail.com>
2022-08-31 13:20:37 +0200

    basic structure of the page done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :recipes, class_name: 'Recipe', foreign_key: :creator_id
+  has_many :participations
+  has_many :circles, through: :participations
   has_many :comments
 
   validates :first_name, length: { maximum: 15 }
