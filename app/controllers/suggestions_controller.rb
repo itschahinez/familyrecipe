b044bfdd13e73a0aad6925d97372f547e9c11fb8
Chahinez Haddad <mackychahinez@gmail.com>
2022-09-05 10:38:56 +0200

    created pseudocode for index and show

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,7 +1,19 @@
 class SuggestionsController < ApplicationController
+  require "json"
+  require "open-uri"
+
+
   def index
+    # get data from the search option
+    # inject it into the search url of the API (Search by ingredients) - ignorePantry=& true, ranking=1
+    # for each of the 10 results, get the name, picture and prep time (API call to get recipe info)
   end
 
   def show
+    # for the recipe we inject the ID into the search URL of the API (Get analyzed recipe ) for instructions
+    # for the recipe we inject the ID into the search URL of the API (get picture, ready in minutes, ingredient list, category) for instructions (change validations on category and maybe units)
+    # change the appropriate fields
+    # incorporate the save option, formulaire cache?
+    # ingredients and quantities are in Search recipes by ingredients
   end
 end
