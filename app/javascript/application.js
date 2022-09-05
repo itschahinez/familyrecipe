b180c3d1509f0bc4b01781324ca3ef264e9546e1
Olivia Bonnal <99920845+Oli0li@users.noreply.github.com>
2022-09-05 17:25:43 +0200

    Update application.js
    Remove jquery (which is not needed)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Entry point for the build script in your package.json
-//= require jquery3
-//= require jquery_ujs
-//= require_tree .
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
